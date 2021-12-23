009b7e16763de1ca07d38dd0d98fdb16c8482f46
Pedro Porcelli <galaxycarft.p@gmail.com>
2021-12-23 13:37:27 -0300

    Added HashSR and HashRS

--- hash/hash.java
+++ hash/hash.java
@@ -4,8 +4,13 @@ import java.util.*;
 
 public class hash {
 
-    public static void Hash(String full_name, HashMap<String, Random> hashCreator) {
+    public static void HashSR(String full_name, HashMap<String, Random> hashCreator) {
         Random hsh = new Random();
         hashCreator.put(full_name, hsh);
+    }
+
+    public static void HashRS(String full_name, HashMap<Random, String> hashCreator) {
+        Random hshInv = new Random();
+        hashCreator.put(hshInv, full_name);
     }
 }
