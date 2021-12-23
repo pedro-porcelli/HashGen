993828acf14e057a2e9b5130439a437f3cf3ae1b
Pedro Porcelli <galaxycarft.p@gmail.com>
2021-12-23 18:50:21 -0300

    added string versions

--- hash/hash.java
+++ hash/hash.java
@@ -13,4 +13,16 @@ public class hash {
         Random hshInv = new Random();
         hashCreator.put(hshInv, full_name);
     }
+
+    public static void HashRSstring(String full_name, HashMap<String, String> hashCreator) {
+        Random hshInv = new Random();
+        String hshInvString = hshInv.toString();
+        hashCreator.put(hshInvString, full_name);
+    }
+
+    public static void HashSRstring(String full_name, HashMap<String, String> hashCreator) {
+        Random hshInv = new Random();
+        String hshInvString = hshInv.toString();
+        hashCreator.put(full_name, hshInvString);
+    }
 }
