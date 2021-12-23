c8d29458414e4ac38c0ba3354e7ccd8e658ec06a
Pedro Porcelli <galaxycarft.p@gmail.com>
2021-12-23 09:32:20 -0300

    I changed it to put in full name

---
+++ README.md
@@ -0,0 +1,3 @@
+# HashMaps
+
+Makes a hash with a hashmap and associates it with your name as a value to the hash key.
--- hash/hash.java
+++ hash/hash.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class hash {
 
-    public static void Hash(String first_name, HashMap<Random, String> hashCreator) {
+    public static void Hash(String full_name, HashMap<Random, String> hashCreator) {
         Random hsh = new Random();
-        hashCreator.put(hsh, first_name);
+        hashCreator.put(hsh, full_name);
     }
 }
