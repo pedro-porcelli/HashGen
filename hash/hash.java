3c793311b27f999e77280da956daef4965f28e60
Pedro Porcelli <galaxycarft.p@gmail.com>
2021-12-23 09:37:09 -0300

    Changed so you can type name to get hash

--- hash/hash.java
+++ hash/hash.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class hash {
 
-    public static void Hash(String full_name, HashMap<Random, String> hashCreator) {
+    public static void Hash(String full_name, HashMap<String, Random> hashCreator) {
         Random hsh = new Random();
-        hashCreator.put(hsh, full_name);
+        hashCreator.put(full_name, hsh);
     }
 }
