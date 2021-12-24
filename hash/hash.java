901bdced87ea199031ff967ca8352fce14064f50
Pedro Porcelli <galaxycarft.p@gmail.com>
2021-12-24 20:24:48 -0300

    added server hash

--- hash/hash.java
+++ hash/hash.java
@@ -1,28 +1,44 @@
 package HashGen.hash;
 
+import java.net.*;
 import java.util.*;
+import java.io.*;
 
 public class hash {
 
-    public static void HashSR(String full_name, HashMap<String, Random> hashCreator) {
+    Socket clientSocket;
+    PrintWriter out;
+    BufferedReader in;
+
+    public static Random HashSR(String full_name, HashMap<String, Random> hashCreator) {
         Random hsh = new Random();
         hashCreator.put(full_name, hsh);
+        return hsh;
     }
 
-    public static void HashRS(String full_name, HashMap<Random, String> hashCreator) {
+    public static Random HashRS(String full_name, HashMap<Random, String> hashCreator) {
         Random hshInv = new Random();
         hashCreator.put(hshInv, full_name);
+        return hshInv;
     }
 
-    public static void HashRSstring(String full_name, HashMap<String, String> hashCreator) {
+    public static String HashRSstring(String full_name, HashMap<String, String> hashCreator) {
         Random hshInv = new Random();
         String hshInvString = hshInv.toString();
         hashCreator.put(hshInvString, full_name);
+        return hshInvString;
     }
 
-    public static void HashSRstring(String full_name, HashMap<String, String> hashCreator) {
+    public static String HashSRstring(String full_name, HashMap<String, String> hashCreator) {
         Random hshInv = new Random();
         String hshInvString = hshInv.toString();
         hashCreator.put(full_name, hshInvString);
+        return hshInvString;
+    }
+
+    public void serverHash(String ipAddress, int port, HashMap<Object, Object> hashSrv) {
+        clientSocket = new Socket(ipAddress, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        out.println(hashSrv);
     }
 }
